eb6b5e6364215b4cb1e5b24938cc021a4f67d66e
github-action-benchmark <github@users.noreply.github.com>
2025-12-11 06:30:37 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for a2d5f3205e760cffbb5b5f177aaddb6e74712d53

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765434149951,
+  "lastUpdate": 1765434637596,
   "entries": {
     "Rust Benchmark": [
       {
@@ -30285,6 +30285,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000013923255382144348",
             "extra": "mean: 1.6573674933549272 msec\nrounds: 602"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "77726495+VaradaNambiar@users.noreply.github.com",
+            "name": "VaradaNambiar",
+            "username": "VaradaNambiar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a2d5f3205e760cffbb5b5f177aaddb6e74712d53",
+          "message": "Merge pull request #16 from arup-group/fix-codescanning-alerts-for-github-permission\n\nFix codescanning alerts for GitHub permission",
+          "timestamp": "2025-12-11T12:00:15+05:30",
+          "tree_id": "331b636e0e6a9be7ed5d39ee686dfcdb0f340487",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/a2d5f3205e760cffbb5b5f177aaddb6e74712d53"
+        },
+        "date": 1765434635961,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 76120.14289278335,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000013073069725801367",
+            "extra": "mean: 13.137127204405262 usec\nrounds: 37538"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 622.3701790034352,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000014477687813354897",
+            "extra": "mean: 1.6067607892158993 msec\nrounds: 612"
           }
         ]
       }
