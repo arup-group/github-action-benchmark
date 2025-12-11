20f73a480b21d466f7654b737478273fac6666f6
github-action-benchmark <github@users.noreply.github.com>
2025-12-11 09:02:09 +0000

    add Go Benchmark (go) benchmark result for a2c64ea8bb87f524a3d63e622a55f6d9bb0719dc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765443727882,
+  "lastUpdate": 1765443729096,
   "entries": {
     "Rust Benchmark": [
       {
@@ -16637,6 +16637,48 @@ window.BENCHMARK_DATA = {
             "value": 39900,
             "unit": "ns/op\t         4.000 auxMetricUnits",
             "extra": "30237 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "60345260+RaviKumarGubbala@users.noreply.github.com",
+            "name": "Ravi Kumar Gubbala",
+            "username": "RaviKumarGubbala"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a2c64ea8bb87f524a3d63e622a55f6d9bb0719dc",
+          "message": "Merge pull request #19 from arup-group/fix-codescan-slerts-for-github-permission-in-workflow\n\nadd permissions for jobs in workflows of ci.yml file",
+          "timestamp": "2025-12-11T14:31:41+05:30",
+          "tree_id": "3362ed30beb4b4c48d0df97ebd21cad8d34d4968",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/a2c64ea8bb87f524a3d63e622a55f6d9bb0719dc"
+        },
+        "date": 1765443726332,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib10",
+            "value": 312.9,
+            "unit": "ns/op",
+            "extra": "3848485 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20",
+            "value": 39819,
+            "unit": "ns/op",
+            "extra": "30198 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric",
+            "value": 39892,
+            "unit": "ns/op\t         4.000 auxMetricUnits",
+            "extra": "29930 times\n4 procs"
           }
         ]
       }
