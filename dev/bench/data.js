fe1e4cabc30d3f7db5444f603386ae9c1a295bdd
github-action-benchmark <github@users.noreply.github.com>
2025-12-11 08:34:46 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 828b08933d91bb820bb09a5afa1133bc135ac15a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765442085297,
+  "lastUpdate": 1765442086546,
   "entries": {
     "Rust Benchmark": [
       {
@@ -30856,6 +30856,44 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/arup-group/github-action-benchmark/commit/828b08933d91bb820bb09a5afa1133bc135ac15a"
         },
         "date": 1765442080281,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 76801.46356472967,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000011225132333972113",
+            "extra": "mean: 13.020585202223156 usec\nrounds: 36073"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 625.7879470094231,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002684008991764681",
+            "extra": "mean: 1.5979853954984244 msec\nrounds: 622"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "77726495+VaradaNambiar@users.noreply.github.com",
+            "name": "VaradaNambiar",
+            "username": "VaradaNambiar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "828b08933d91bb820bb09a5afa1133bc135ac15a",
+          "message": "Merge pull request #18 from arup-group/fix-codescan-slerts-for-github-permission-in-workflow\n\nFix codescan alerts for GitHub permission in workflow",
+          "timestamp": "2025-12-11T14:04:16+05:30",
+          "tree_id": "1b9bca2ef89b1e4249672f2e882dc51a9e8961b7",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/828b08933d91bb820bb09a5afa1133bc135ac15a"
+        },
+        "date": 1765442083857,
         "tool": "pytest",
         "benches": [
           {
