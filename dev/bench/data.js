943e492916bf42d29c4167b9540729464f8b4ff3
github-action-benchmark <github@users.noreply.github.com>
2026-05-17 11:39:35 +0000

    add Julia benchmark result (julia) benchmark result for 332227ea7e68a0fecf3eaf5c47dba883cd7e848d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779017954436,
+  "lastUpdate": 1779017975246,
   "entries": {
     "Rust Benchmark": [
       {
@@ -48627,6 +48627,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "fib/20",
             "value": 43372,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47802+renefritze@users.noreply.github.com",
+            "name": "René Fritze",
+            "username": "renefritze"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "332227ea7e68a0fecf3eaf5c47dba883cd7e848d",
+          "message": "Merge pull request #38 from arup-group/dependabot/pip/examples/pytest/pytest-9.0.3\n\nchore(deps): bump pytest from 5.2.4 to 9.0.3 in /examples/pytest",
+          "timestamp": "2026-05-17T13:38:34+02:00",
+          "tree_id": "28d139cc73ffef0d879fe60f068c2a03838a5ecf",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/332227ea7e68a0fecf3eaf5c47dba883cd7e848d"
+        },
+        "date": 1779017971570,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "fib/10",
+            "value": 283.92932862190816,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":283,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "fib/20",
+            "value": 35806,
             "unit": "ns",
             "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
