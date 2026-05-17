0af47e1e89bda2c93b70b69711833784c11ac051
github-action-benchmark <github@users.noreply.github.com>
2026-05-17 11:36:06 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 95f9a93867cc2a004fff9995f704637c95a1c1eb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779017762501,
+  "lastUpdate": 1779017766806,
   "entries": {
     "Rust Benchmark": [
       {
@@ -50339,6 +50339,42 @@ window.BENCHMARK_DATA = {
             "value": 43.55626073877017,
             "unit": "ns",
             "range": "± 0.039221021107239074"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47802+renefritze@users.noreply.github.com",
+            "name": "René Fritze",
+            "username": "renefritze"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "95f9a93867cc2a004fff9995f704637c95a1c1eb",
+          "message": "Merge pull request #37 from arup-group/dependabot/npm_and_yarn/picomatch-2.3.2\n\nchore(deps-dev): bump picomatch from 2.3.1 to 2.3.2",
+          "timestamp": "2026-05-17T13:34:50+02:00",
+          "tree_id": "41a0bf19927f189dbf538245bb3a43bfff4dab2a",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/95f9a93867cc2a004fff9995f704637c95a1c1eb"
+        },
+        "date": 1779017763576,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Sample.Benchmarks.Fib10",
+            "value": 18.86713019652026,
+            "unit": "ns",
+            "range": "± 0.006184724666395949"
+          },
+          {
+            "name": "Sample.Benchmarks.Fib20",
+            "value": 37.49945355607913,
+            "unit": "ns",
+            "range": "± 0.031025499951725127"
           }
         ]
       }
