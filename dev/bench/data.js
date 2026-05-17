f4ba4a0942322b2c2f1db2bb421d83a82efe3949
github-action-benchmark <github@users.noreply.github.com>
2026-05-17 11:39:14 +0000

    add Catch2 Benchmark (catch2) benchmark result for 332227ea7e68a0fecf3eaf5c47dba883cd7e848d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779017952254,
+  "lastUpdate": 1779017954436,
   "entries": {
     "Rust Benchmark": [
       {
@@ -46162,6 +46162,44 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/arup-group/github-action-benchmark/commit/332227ea7e68a0fecf3eaf5c47dba883cd7e848d"
         },
         "date": 1779017947150,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 85.8766,
+            "range": "± 7.3849",
+            "unit": "ns",
+            "extra": "100 samples\n358 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 8.01719,
+            "range": "± 472.67",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47802+renefritze@users.noreply.github.com",
+            "name": "René Fritze",
+            "username": "renefritze"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "332227ea7e68a0fecf3eaf5c47dba883cd7e848d",
+          "message": "Merge pull request #38 from arup-group/dependabot/pip/examples/pytest/pytest-9.0.3\n\nchore(deps): bump pytest from 5.2.4 to 9.0.3 in /examples/pytest",
+          "timestamp": "2026-05-17T13:38:34+02:00",
+          "tree_id": "28d139cc73ffef0d879fe60f068c2a03838a5ecf",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/332227ea7e68a0fecf3eaf5c47dba883cd7e848d"
+        },
+        "date": 1779017953855,
         "tool": "catch2",
         "benches": [
           {
