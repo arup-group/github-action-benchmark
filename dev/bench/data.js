9dc5f11a1a34c26b9df81b50bf0f086d3e44274b
github-action-benchmark <github@users.noreply.github.com>
2026-07-10 11:36:37 +0000

    add Catch2 Benchmark (catch2) benchmark result for a2e37da82e7a6a600c08123261c642b84578f15d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783683394672,
+  "lastUpdate": 1783683397897,
   "entries": {
     "Rust Benchmark": [
       {
@@ -46753,6 +46753,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.70883,
             "range": "± 453.962",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47802+renefritze@users.noreply.github.com",
+            "name": "René Fritze",
+            "username": "renefritze"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a2e37da82e7a6a600c08123261c642b84578f15d",
+          "message": "Merge pull request #41 from arup-group/dependabot/npm_and_yarn/markdown-it-14.2.0\n\nchore(deps-dev): bump markdown-it from 12.3.2 to 14.2.0",
+          "timestamp": "2026-07-10T13:35:55+02:00",
+          "tree_id": "9bc186a77e6e6e77845fe6a664d7b8efe938ee9b",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/a2e37da82e7a6a600c08123261c642b84578f15d"
+        },
+        "date": 1783683390502,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 80.6144,
+            "range": "± 4.06166",
+            "unit": "ns",
+            "extra": "100 samples\n360 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.58856,
+            "range": "± 907.935",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }
