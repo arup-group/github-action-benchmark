d2dfedb6fd65939038d91bf272ebc69f99cc3b94
github-action-benchmark <github@users.noreply.github.com>
2026-08-07 14:19:46 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for c1983a5c9f7082adc28790659c0f25cfa26206ac

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786112377664,
+  "lastUpdate": 1786112386279,
   "entries": {
     "Rust Benchmark": [
       {
@@ -52170,6 +52170,42 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/arup-group/github-action-benchmark/commit/c1983a5c9f7082adc28790659c0f25cfa26206ac"
         },
         "date": 1786112366894,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Sample.Benchmarks.Fib10",
+            "value": 21.27669717027591,
+            "unit": "ns",
+            "range": "± 0.026332571507247677"
+          },
+          {
+            "name": "Sample.Benchmarks.Fib20",
+            "value": 43.06149565256559,
+            "unit": "ns",
+            "range": "± 0.05182059650984153"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47802+renefritze@users.noreply.github.com",
+            "name": "René Fritze",
+            "username": "renefritze"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c1983a5c9f7082adc28790659c0f25cfa26206ac",
+          "message": "Merge pull request #44 from arup-group/renefritze-patch-1\n\nConfigure Dependabot for GitHub Actions and npm",
+          "timestamp": "2026-08-07T07:18:20-07:00",
+          "tree_id": "2a4416521002e58ee43e871cd5ca035f6e6c1a68",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/c1983a5c9f7082adc28790659c0f25cfa26206ac"
+        },
+        "date": 1786112374712,
         "tool": "benchmarkdotnet",
         "benches": [
           {
