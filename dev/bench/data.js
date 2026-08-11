51e986d73b79c34ca121bcc4597547a6c1dd6c3f
github-action-benchmark <github@users.noreply.github.com>
2026-08-11 06:44:45 +0000

    add C++ Benchmark (googlecpp) benchmark result for 117b62a8801fdf295d44eeb104383975a291a657

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786115501741,
+  "lastUpdate": 1786430685909,
   "entries": {
     "Rust Benchmark": [
       {
@@ -42467,6 +42467,42 @@ window.BENCHMARK_DATA = {
             "value": 22341.473147822828,
             "unit": "ns/iter",
             "extra": "iterations: 31301\ncpu: 22336.654324143 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47802+renefritze@users.noreply.github.com",
+            "name": "René Fritze",
+            "username": "renefritze"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "117b62a8801fdf295d44eeb104383975a291a657",
+          "message": "Merge pull request #45 from arup-group/dependabot/npm_and_yarn/all_npm-33a0ec8f65\n\nchore(deps): bump the all_npm group across 1 directory with 23 updates",
+          "timestamp": "2026-08-11T08:44:18+02:00",
+          "tree_id": "8f40d9e576c8d64c9a3b7814a07a80495dbc03c7",
+          "url": "https://github.com/arup-group/github-action-benchmark/commit/117b62a8801fdf295d44eeb104383975a291a657"
+        },
+        "date": 1786430681842,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "fib_10",
+            "value": 200.1936270602536,
+            "unit": "ns/iter",
+            "extra": "iterations: 3506545\ncpu: 200.16322705112867 ns\nthreads: 1"
+          },
+          {
+            "name": "fib_20",
+            "value": 24733.985444782025,
+            "unit": "ns/iter",
+            "extra": "iterations: 28306\ncpu: 24731.539108316258 ns\nthreads: 1"
           }
         ]
       }
